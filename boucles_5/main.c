e568bb8e18dc85dc361c66f814b18453e0009f7a
yassir346 <yassir.mahir98@gmail.com>
2024-09-26 13:31:27 +0100

    commit after change

--- boucles_5/main.c
+++ boucles_5/main.c
@@ -3,6 +3,19 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int Base, Expo, Result = 1, i;
+
+    printf("la base = ");
+    scanf("%d", &Base);
+    printf("\nl'expo = ");
+    scanf("%d", &Expo);
+
+    for (i = 1;i <= Expo; i++)
+    {
+        Result = (Result*Base);
+    }
+
+    printf("la puissance est : %d", Result);
+
     return 0;
 }
